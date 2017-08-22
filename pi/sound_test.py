acee466de7fa77bb64ee8602658022ca05c55143
Lucas <lucas.b.almeida@live.com>
2017-08-22 14:09:09 -0300

    some python code changes (structure and sounds)

--- pi/sound_test.py
+++ pi/sound_test.py
@@ -1,6 +1,19 @@
 import pygame
+import time
+from time import sleep
+
+import winsound
+
 pygame.mixer.init()
-pygame.mixer.music.load("bemvindo.wav")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+def playsound(sound, soundtime):
+    pygame.mixer.music.load(sound)
+    pygame.mixer.music.set_endevent(pygame.constants.USEREVENT)
+    pygame.mixer.music.play()
+    sleep(soundtime)
+
+
+playsound("takepic_again.wav",9)
+playsound("calling_owner.wav",5)
+playsound("norecog.wav",9)
+playsound("acess_denied.wav",4)
+playsound("acess_granted.wav",4)
